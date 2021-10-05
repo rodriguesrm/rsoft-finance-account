6709acc09717dab3414c116c1640b94d9dccf6f6
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2021-10-05 13:45:49 -0300

    added docker-support

--- src/RSoft.Entry.Tests/Stubs/ConsumeContextStub.cs
+++ src/RSoft.Entry.Tests/Stubs/ConsumeContextStub.cs
@@ -48,65 +48,49 @@ namespace RSoft.Entry.Tests.Stubs
         public Headers Headers { get; set; }
         public HostInfo Host { get; set; }
 
-        public void AddConsumeTask(Task task)
-        {
-            throw new NotImplementedException();
-        }
+        public void AddConsumeTask(Task task) { }
 
         public T1 AddOrUpdatePayload<T1>(PayloadFactory<T1> addFactory, UpdatePayloadFactory<T1> updateFactory) where T1 : class
         {
-            throw new NotImplementedException();
+            T1 result = Activator.CreateInstance<T1>();
+            return result;
         }
 
         public ConnectHandle ConnectPublishObserver(IPublishObserver observer)
-        {
-            throw new NotImplementedException();
-        }
+            => default;
 
         public ConnectHandle ConnectSendObserver(ISendObserver observer)
-        {
-            throw new NotImplementedException();
-        }
+            => default;
 
         public T1 GetOrAddPayload<T1>(PayloadFactory<T1> payloadFactory) where T1 : class
         {
-            throw new NotImplementedException();
+            T1 result = Activator.CreateInstance<T1>();
+            return result;
         }
 
         public Task<ISendEndpoint> GetSendEndpoint(Uri address)
         {
-            throw new NotImplementedException();
+            ISendEndpoint result = default;
+            return Task.FromResult(result);
         }
 
         public bool HasMessageType(Type messageType)
-        {
-            throw new NotImplementedException();
-        }
+            => true;
 
         public bool HasPayloadType(Type payloadType)
-        {
-            throw new NotImplementedException();
-        }
+            => true;
 
         public Task NotifyConsumed(TimeSpan duration, string consumerType)
-        {
-            throw new NotImplementedException();
-        }
+            => Task.CompletedTask;
 
         public Task NotifyConsumed<T1>(ConsumeContext<T1> context, TimeSpan duration, string consumerType) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
+            => Task.CompletedTask;
 
         public Task NotifyFaulted(TimeSpan duration, string consumerType, Exception exception)
-        {
-            throw new NotImplementedException();
-        }
+            => Task.CompletedTask;
 
         public Task NotifyFaulted<T1>(ConsumeContext<T1> context, TimeSpan duration, string consumerType, Exception exception) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
+            => Task.CompletedTask;
 
         public Task Publish<T1>(T1 message, CancellationToken cancellationToken = default) where T1 : class
             => Task.CompletedTask;
@@ -144,10 +128,7 @@ namespace RSoft.Entry.Tests.Stubs
             return Task.FromResult(result);
         }
 
-        public void Respond<T1>(T1 message) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
+        public void Respond<T1>(T1 message) where T1 : class { }
 
         public Task RespondAsync<T1>(T1 message) where T1 : class
             => Task.CompletedTask;
@@ -183,9 +164,7 @@ namespace RSoft.Entry.Tests.Stubs
         }
 
         public Task RespondAsync<T1>(object values, IPipe<SendContext<T1>> sendPipe) where T1 : class
-        {
-            throw new NotImplementedException();
-        }
+            => Task.CompletedTask;
 
         public Task RespondAsync<T1>(object values, IPipe<SendContext> sendPipe) where T1 : class
         {
@@ -195,12 +174,14 @@ namespace RSoft.Entry.Tests.Stubs
 
         public bool TryGetMessage<T1>(out ConsumeContext<T1> consumeContext) where T1 : class
         {
-            throw new NotImplementedException();
+            consumeContext = default;
+            return true;
         }
 
         public bool TryGetPayload<T1>(out T1 payload) where T1 : class
         {
-            throw new NotImplementedException();
+            payload = default;
+            return true;
         }
     }
 }
